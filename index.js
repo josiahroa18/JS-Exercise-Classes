c91ef8f447edef1fb9d00a1754865fc7190e5f46
Josiah Roa <josiahroa18@gmail.com>
2020-01-30 12:59:21 -0700

    Completed MVP

--- index.js
+++ index.js
@@ -196,8 +196,18 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor(atr){
+    super(atr);
+    this.gradClassName = atr.gradClassName;
+    this.favInstructor = atr.favInstructor;
+  }
+  standUp(slackChannel){
+    return `${this.name} announces to ${slackChannel}, @channel standy times!`;
+  }
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+  }
 }
 
 /*
